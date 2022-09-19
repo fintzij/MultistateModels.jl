057a57ebbe00fd58b3564154a81aeba33853d16d
Jon Fintzi <fintzijr@nih.gov>
2022-09-19 11:55:16 -0400

    diffeq

--- src/modelfitting.jl
+++ src/modelfitting.jl
@@ -6,9 +6,10 @@ Fit a model.
 function fit(model::MultistateModel; alg = "ml")
     
     # if sample paths are fully observed, maximize the likelihood directly
-    # Note: not doing obstype 0 for a while
     if all(model.data.obstype .== 1)
         fitted = fit_exact(model)
+    elseif all(model.data.obstype .== 2) & all(isa.(model.hazards, MultistateModels._Exponential))
+        fitted = fit_interval(model)
     end
 
     # return fitted object
@@ -40,4 +41,19 @@ function fit_exact(model::MultistateModel)
 
     # wrap results
     
+end
+
+
+
+"""
+    fit_interval(model::MultistateModel)
+
+Fit a multistate model given all observed paths are interval censored (i.e. model.data.obstype .== 2) AND all hazards are exponential
+"""
+function fit_interval(model::MultistateModel)
+
+    # parse the data and hazards to generate the container of transition probability matrices
+    # for now, we don't have time-inhomogeneous smoothly varying baseline cause-specific hazards
+    # 
+
 end
--- test/setup_2statetrans_dat.jl
+++ test/setup_2statetrans_dat.jl
@@ -8,27 +8,27 @@ h12 = Hazard(@formula(0 ~ 1), "exp", 1, 2)
 h21 = Hazard(@formula(0 ~ 1), "exp", 2, 1)
 
 dat = 
-    DataFrame(id = collect(1:100),
-              tstart = fill(0.0, 100),
-              tstop = fill(10.0, 100),
-              statefrom = fill(1, 100),
-              stateto = fill(2, 100),
-              obstype = fill(1, 100))
+    DataFrame(id = repeat(collect(1:100), inner = 2),
+              tstart = repeat([0.0,5.0], outer = 100),
+              tstop = repeat([5.0,10.0], outer = 100),
+              statefrom = fill(1, 200),
+              stateto = fill(2, 200),
+              obstype = fill(2, 200))
 
 
-append!(dat, DataFrame(id=1,tstart=10.0,tstop=20.0,statefrom=2,stateto=1,obstype=2))
-append!(dat, DataFrame(id=1,tstart=20.0,tstop=30.0,statefrom=2,stateto=1,obstype=3))
-sort!(dat, [:id,])
+# append!(dat, DataFrame(id=1,tstart=10.0,tstop=20.0,statefrom=2,stateto=1,obstype=2))
+# append!(dat, DataFrame(id=1,tstart=20.0,tstop=30.0,statefrom=2,stateto=1,obstype=3))
+# sort!(dat, [:id,])
 
 # create multistate model object
 msm_2state_trans = multistatemodel(h12, h21; data = dat)
 
 # set model parameters
-# want mean time to event of 5, so log(1/5) = log(0.2). Hazard ratio of 1.3, so log(1.3)
+# want mean time to event of 5
 set_parameters!(
     msm_2state_trans, 
     (h12 = [log(0.2)],
-     h21 = [log(0.5)]))
+     h21 = [log(0.2)]))
 
 simdat, paths = simulate(msm_2state_trans; paths = true, data = true);
 
