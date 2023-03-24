c61697d934bcdd2e34fe5086e0d6bea5f9ed734a
liangcj <liangcj@gmail.com>
2023-03-24 13:02:44 -0400

    calculuate_crude() for crude parame initialization
    Co-authored-by: Jon Fintzi <fintzijr@nih.gov>

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -1,3 +1,58 @@
+# store parameters in tuple or vector of vectors
+# collect time spent in each state
+#  store in vector of time in states T_r (msm R notation)
+# collect transitions
+#  store in matrix like tmat, refactor compute_statetable() and statetable()
+
+# loop through hazards in model
+# loop through data to collect events/time at risk
+
+#m2 = deepcopy(model)
+#initpar = deepcopy(m2.parameters)
+#T_r = zeros(size(m2.tmat)[1])
+#n_rs = zeros(size(m2.tmat))
+
+function calculate_crude(model::MultistateModel)
+    usubj = unique(model.data.id)
+    T_r = zeros(size(model.tmat)[1])
+    n_rs = zeros(size(model.tmat))
+
+    for s in usubj
+        # loop through all unique subjects
+        
+        # container for accumulating T_r
+        T_r_accumulator = 0
+        for r in eachrow(model.data[findall(model.data.id .== s), :])
+            # loop through data rows for eachc subject
+            
+            # track how much time has been spent in state
+            T_r_accumulator += r.tstop - r.tstart
+            #T_r[r.statefrom] += r.tstop - r.tstart
+            if(r.statefrom != r.stateto)
+                # if a state transition happens then increment time spent in state in T_r
+                # and increment transition by 1 in n_rs
+                # if no transition happens then accumulated state time is ignored
+                T_r[r.statefrom] += T_r_accumulator
+                T_r_accumulator = 0
+                n_rs[r.statefrom, r.stateto] += 1
+            end
+        end
+    end
+    return n_rs ./ T_r
+end
+
+# take matrix of crude exponential rates
+# if exp then plug in
+# if exp with covariates plug in and center covariates
+# if weibull
+# if weibull with covariates
+# if Gompertz
+# if Gompertz with covariates
+
+#exp_rates = MultistateModels.calculate_crude(model)
+
+
+
 """ 
     compute_statetable(dat, tmat)
 
@@ -56,7 +111,6 @@ end
 - model: MultistateModel object
 - groups: different categorical variables to subset on (e.g. male, trt, etc.)
 """
-
 function initialize_parameters(model::MultistateModel, groups::Symbol...)
     # obtain data from multistatemodel object
     data = model.hazards[1].data
@@ -97,6 +151,3 @@ function crudeinit(transmat, tmat)
     # return the matrix of initial intensity values
     return q_crude_mat
 end
-
-
-
