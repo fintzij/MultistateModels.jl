fc6af576c10b80a743c90ce53d7f46744c89dc41
Raphael Morsomme <rmorsomme@gmail.com>
2023-07-11 14:13:06 -0400

    summary table

--- scratch/simulation.jl
+++ scratch/simulation.jl
@@ -5,19 +5,26 @@ using MultistateModels
 
 #
 # Model setup for simulation
+
+# healthy-ill-dead model
 h12 = Hazard(@formula(0 ~ 1), "exp", 1, 2) # healthy -> ill
-h21 = Hazard(@formula(0 ~ 1), "exp", 2, 1) # ill -> healthy
 h13 = Hazard(@formula(0 ~ 1), "exp", 1, 3) # healthy -> dead
+h21 = Hazard(@formula(0 ~ 1), "exp", 2, 1) # ill -> healthy
 h23 = Hazard(@formula(0 ~ 1), "exp", 2, 3) # ill -> dead
 
-nsubj = 10
+# observation window
+tstart = 0.0
+tstop = 3.0
+
+# subject data
+nsubj = 10000 # number of subject
 dat = 
-    DataFrame(id = collect(1:nsubj),
-              tstart = fill(0.0, nsubj),
-              tstop = fill(3.0, nsubj),
-              statefrom = fill(1, nsubj),
-              stateto = fill(2, nsubj),
-              obstype = fill(1, nsubj))
+    DataFrame(id = collect(1:nsubj), # subject id
+              tstart = fill(tstart, nsubj),
+              tstop = fill(tstop, nsubj),
+              statefrom = fill(1, nsubj), # state at time `tstart`
+              stateto = fill(2, nsubj), # `stateto` is a placeholder before the simulation
+              obstype = fill(1, nsubj)) # `obstype=1` indicates exactly observed data
 
 # create multistate model object
 model = multistatemodel(h12, h13, h21, h23; data = dat)
@@ -26,9 +33,9 @@ model = multistatemodel(h12, h13, h21, h23; data = dat)
 set_parameters!(
     model, 
     (h12 = [log(1.1)],
+     h13 = [log(0.3)],
      h21 = [log(0.7)],
-     h13 = [log(0.2)],
-     h23 = [log(0.8)]))
+     h23 = [log(0.9)]))
 
 #
 # Simulate data
@@ -40,4 +47,10 @@ simdat[1]
 # build model with simulated data
 model = multistatemodel(h12, h13, h21, h23; data = simdat[1])
 
+# fit model
+# slow the first time because of compilation
+# much faster the subsequent runs
 model_fitted=fit(model)
+
+summary_table, ll, AIC, BIC = MultistateModels.summary(model_fitted)
+println(summary_table[:h12]) # MLE and 95% confidence intervals.
--- src/helpers.jl
+++ src/helpers.jl
@@ -187,12 +187,12 @@ end
 
 function check_SamplingWeights(SamplingWeights::Vector{Float64}, data::DataFrame)
     
-    # check that the number of weights is correct
+    # check that the number of sampling weights is correct
     if length(SamplingWeights) != length(unique(data.id))
         error("The length of SamplingWeights is not equal to the number of subjects.")
     end
 
-    # check that the weights are non-negative
+    # check that the sampling weights are non-negative
     if any(SamplingWeights .<= 0)
         error("The elements of SamplingWeights should be non-negative.")
     end
@@ -433,28 +433,48 @@ Return the variance covariance matrix at the maximum likelihood estimate.
 
 # Arguments 
 - `model::MultistateModelFitted`: fitted model
+- `confidence_level::Float64`: confidence level of the confidence intervals
 """
-function summary(model::MultistateModelFitted) 
+function summary(model::MultistateModelFitted; confidence_level::Float64 = 0.95) 
 
-    # maximum likelihood estimates
-    est = estimates(model)
-
-    # confidence intervals
-    vcov = vcov(model)
+    #
+    # summary table
+    
+    # standard error
+    vcov = model.vcov
     se = sqrt.(vcov[diagind(vcov)])
-    lb =est.-1.96.*se
-    ub =est.+1.96.*se
-    CI = exp.(DataFrame(lower = lb, estimate = est, upper = ub))    
+    se_vv = VectorOfVectors(se, model.parameters.elem_ptr)
 
+    # name of hazards
+    haznames = map(x -> model.hazards[x].hazname, collect(1:length(model.hazards)))
+
+    # table
+    summary_table = Vector{DataFrame}(undef, length(haznames))
+    z_critical = quantile(Normal(0.0, 1.0), 1-(1-confidence_level)/2)
+    for s in eachindex(summary_table)
+        # summary for hazard s
+        summary_table[s] = DataFrame(
+            estimate = reduce(vcat, model.parameters[s]),
+            se = reduce(vcat, se_vv[s]))
+
+        summary_table[s].upper = summary_table[s].estimate .+ z_critical .* summary_table[s].se
+        summary_table[s].lower = summary_table[s].estimate .- z_critical .* summary_table[s].se
+    end
+
+    # add hazard names to the table
+    summary_table = (;zip(haznames, summary_table)...)
+    
+    #
     # log likelihood
     ll = loglik(model)
 
+    #
     # information criteria
     p = length(estimates(model))
     n = nrow(model.data)
     AIC = -2*ll + 2     *p
     BIC = -2*ll + log(n)*p
 
-    return CI, ll, AIC, BIC
+    return summary_table, ll, AIC, BIC
 
 end
