4dd5c33e3f8ffda9ba2fc02ef18eeb0c3d3c62f5
Raphael Morsomme <rmorsomme@gmail.com>
2023-07-21 16:40:21 -0400

    vignette - simulation with covariate (draft)

--- scratch/simulation.jl
+++ scratch/simulation.jl
@@ -128,3 +128,50 @@ model_fitted = fit(model)
 summary_table, ll, AIC, BIC = MultistateModels.summary(model_fitted)
 println(summary_table[:h12])
 println(par_true[:h12]) # true value
+
+
+#
+# Covariates
+
+# include a treatment effect for some transitions
+h12 = Hazard(@formula(0 ~ 1 + trt), "exp", 1, 2) # healthy -> ill
+h13 = Hazard(@formula(0 ~ 1), "exp", 1, 3) # healthy -> dead
+h21 = Hazard(@formula(0 ~ 1 + trt), "exp", 2, 1) # ill -> healthy
+h23 = Hazard(@formula(0 ~ 1 + trt), "exp", 2, 3) # ill -> dead
+
+# parameters
+par_true = (
+    h12 = [log(1.1), log(1/3)], # multiplicative effect of treatmet is 1/3 (protective effect)
+    h13 = [log(0.3)],
+    h21 = [log(0.7), log(2)], # gets healthy twice as quickly if treated
+    h23 = [log(0.9), log(1/1.5)])
+
+# subject data with a binary covariate (trt)
+dat =
+  DataFrame(id        = collect(1:nsubj), # subject id
+            tstart    = fill(tstart, nsubj),
+            tstop     = fill(tstop, nsubj),
+            statefrom = fill(1, nsubj), # state at time `tstart`
+            stateto   = fill(2, nsubj), # `stateto` is a placeholder before the simulation
+            obstype   = fill(1, nsubj), # `obstype=1` indicates exactly observed data
+            trt       = repeat(collect(0:1), outer = Int64(nsubj/2))) # odd subjects receive the control (trt=1) and even subjects receive the treatment (trt=1)
+
+
+
+
+
+
+
+#
+# Semi-Markov with exactly observed data
+
+par_true = (
+    h12 = [log(1.5), log(2)],
+    h13 = [log(2), log(2.5)],
+    h21 = [log(0.7)],
+    h23 = [log(0.9), log(1)])
+
+h12 = Hazard(@formula(0 ~ 1), "wei", 1, 2) # healthy -> ill
+h13 = Hazard(@formula(0 ~ 1), "wei", 1, 3) # healthy -> dead
+h21 = Hazard(@formula(0 ~ 1), "exp", 2, 1) # ill -> healthy
+h23 = Hazard(@formula(0 ~ 1), "gom", 2, 3) # ill -> dead
