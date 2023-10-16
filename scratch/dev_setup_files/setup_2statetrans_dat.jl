33721a4a1a85b6dbbaeae4dc5af2747a2bf41be5
Jon Fintzi <fintzijr@nih.gov>
2023-10-16 17:26:25 -0400

    setup_2statetrans_dat udpate

--- scratch/dev_setup_files/setup_2statetrans_dat.jl
+++ scratch/dev_setup_files/setup_2statetrans_dat.jl
@@ -3,33 +3,49 @@ using DataFrames
 using Distributions
 using MultistateModels
 
-h12 = Hazard(@formula(0 ~ 1), "exp", 1, 2)
+h12 = Hazard(@formula(0 ~ 1), "wei", 1, 2)
 h21 = Hazard(@formula(0 ~ 1), "exp", 2, 1)
 
-dat = 
-    DataFrame(id = repeat(collect(1:100), inner = 2),
-              tstart = repeat([0.0,5.0], outer = 100),
-              tstop = repeat([5.0,10.0], outer = 100),
-              statefrom = fill(1, 200),
-              stateto = fill(2, 200),
-              obstype = fill(2, 200))
-
+nsubj = 100
+ntimes = 5
+dat = DataFrame(id = repeat(collect(1:100), inner = ntimes),
+                tstart = repeat(collect(0:(10/ntimes):(10 - 10/ntimes)), outer = nsubj),
+                tstop = repeat(collect((10/ntimes):(10/ntimes):10), outer = nsubj),
+                statefrom = fill(1, nsubj * ntimes),
+                stateto = fill(2, nsubj * ntimes),
+                obstype = fill(2, nsubj * ntimes))
 
 # append!(dat, DataFrame(id=1,tstart=10.0,tstop=20.0,statefrom=2,stateto=1,obstype=2))
 # append!(dat, DataFrame(id=1,tstart=20.0,tstop=30.0,statefrom=2,stateto=1,obstype=3))
 # sort!(dat, [:id,])
 
 # create multistate model object
-msm_2state_trans = multistatemodel(h12, h21; data = dat)
+model = multistatemodel(h12, h21; data = dat)
 
 # set model parameters
 # want mean time to event of 5
 set_parameters!(
-    msm_2state_trans, 
-    (h12 = [log(0.2)],
-     h21 = [log(0.2)]))
+    model, 
+    (h12 = [log(0.4), log(0.8)],
+     h21 = [log(0.4)]))
 
-simdat, paths = simulate(msm_2state_trans; paths = true, data = true);
+simdat, paths = simulate(model; paths = true, data = true);
 
 # create multistate model object with the simulated data
-msm_2state_trans = multistatemodel(h12, h21; data = simdat[1])
+model = multistatemodel(h12, h21; data = simdat[1])
+
+MultistateModels.set_crude_init!(model)
+
+# errors b/c there is no emat in the model
+fitted = fit(model) 
+
+
+# load libraries and functions
+using ArraysOfArrays, Optimization, Optim, StatsModels, StatsFuns
+
+constraints = nothing; nparticles = 10; maxiter = 100; tol = 1e-4; α = 0.1; γ = 0.05; κ = 1.5;
+surrogate_parameter = nothing; ess_target_initial = 100; MaxSamplingEffort = 10;
+verbose = true; return_ConvergenceRecords = true; return_ProposedPaths = true
+
+using MultistateModels: build_tpm_mapping, MultistateMarkovModel, MultistateMarkovModelCensored, fit
+
