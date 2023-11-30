f2c7cbc2f3f47ae19955b96c82dd0c3d8c753a0f
Jon Fintzi <fintzijr@nih.gov>
2023-11-30 09:40:05 -0500

    Update mcem.jl

--- src/mcem.jl
+++ src/mcem.jl
@@ -31,7 +31,7 @@ function var_ris(l, w)
 
     vris = (sum(l .* w)) ^ 2 * ((sum((w .* l).^2)) / sum(w .* l) ^2 - 2 * (sum(w.^2 .* l) / (sum(w .* l))) + sum(w .^ 2))
 
-    maximum(sqrt(eps(Float64)), vris)
+    vris < sqrt(eps()) ? sqrt(eps()) : vris
 end
 
 """
