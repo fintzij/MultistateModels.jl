a4268f3603c2fe185622da69bbdc6dec7c0bf465
Raphael Morsomme <rmorsomme@gmail.com>
2023-12-23 12:47:40 -0500

    bugfix

--- src/sampling.jl
+++ src/sampling.jl
@@ -6,7 +6,6 @@ Draw additional sample paths until sufficient ess or until the maximum number of
 function DrawSamplePaths!(model::MultistateProcess; ess_target, ess_cur, MaxSamplingEffort, samplepaths, loglik_surrog, loglik_target_prop, loglik_target_cur, ImportanceWeights,tpm_book_surrogate, hazmat_book_surrogate, books, npaths_additional, params_cur, surrogate, psis_pareto_k, fbmats)
 
     for i in eachindex(model.subjectindices)
-        println(i)
         DrawSamplePaths!(i, model; 
             ess_target = ess_target,
             ess_cur = ess_cur, 
