5c1baf8cceca7f39d18a52121af818326e5cd462
Raphael Morsomme <rmorsomme@gmail.com>
2024-01-17 08:16:27 -0500

    clean model output without vcov

--- src/modeloutput.jl
+++ src/modeloutput.jl
@@ -41,7 +41,10 @@ Return the variance covariance matrix at the maximum likelihood estimate.
 # Arguments 
 - `model::MultistateModelFitted`: fitted model
 """
-function get_vcov(model::MultistateModelFitted) 
+function get_vcov(model::MultistateModelFitted)
+    if isnothing(model.vcov)
+        println("The variance-covariance matrix was not computed for this model.")
+    end
     model.vcov
 end
 
@@ -70,29 +73,39 @@ Summary of model output.
 function summary(model::MultistateModelFitted; confidence_level::Float64 = 0.95) 
     
     # maximum likelihood estimates
-    mle=get_parameters(model)
-    
-    # standard error
-    vcov = get_vcov(model)
-    se = sqrt.(vcov[diagind(vcov)])
-    se_vv = VectorOfVectors(se, model.parameters.elem_ptr)
+    mle = get_parameters(model)
 
-    # name of hazards
-    haznames = map(x -> model.hazards[x].hazname, collect(1:length(model.hazards)))
+    # container for summary table
+    summary_table = Vector{DataFrame}(undef, length(model.hazards))
 
-    # summary tables for each hazard
-    summary_table = Vector{DataFrame}(undef, length(haznames))
-    z_critical = quantile(Normal(0.0, 1.0), 1-(1-confidence_level)/2)
-    for s in eachindex(summary_table)
-        # summary for hazard s
-        summary_table[s] = DataFrame(
-            estimate = reduce(vcat, mle[s]),
-            se = reduce(vcat, se_vv[s]))
-        summary_table[s].upper = summary_table[s].estimate .+ z_critical .* summary_table[s].se
-        summary_table[s].lower = summary_table[s].estimate .- z_critical .* summary_table[s].se
+    if isnothing(model.vcov)
+        println("Confidence intervals are not computed for models without a variance-covariance matrix.")
+        # populate summary tables for each hazard
+        for s in eachindex(summary_table)
+            # summary for hazard s (only the estimate when there is no vcov)
+            summary_table[s] = DataFrame(estimate = reduce(vcat, mle[s]))
+        end
+    else    
+        # standard error
+        vcov = get_vcov(model)
+        se = sqrt.(vcov[diagind(vcov)])
+        se_vv = VectorOfVectors(se, model.parameters.elem_ptr)
+        # critical value
+        z_critical = quantile(Normal(0.0, 1.0), 1-(1-confidence_level)/2)
+
+        # populate summary tables for each hazard
+        for s in eachindex(summary_table)
+            # summary for hazard s
+            summary_table[s] = DataFrame(
+                estimate = reduce(vcat, mle[s]),
+                se = reduce(vcat, se_vv[s]))
+            summary_table[s].upper = summary_table[s].estimate .+ z_critical .* summary_table[s].se
+            summary_table[s].lower = summary_table[s].estimate .- z_critical .* summary_table[s].se
+        end
     end
 
     # add hazard names to the table
+    haznames = map(x -> model.hazards[x].hazname, collect(1:length(model.hazards)))
     summary_table = (;zip(haznames, summary_table)...)
     
     # log likelihood
@@ -101,8 +114,8 @@ function summary(model::MultistateModelFitted; confidence_level::Float64 = 0.95)
     # information criteria
     p = length(reduce(vcat, mle))
     n = nrow(model.data)
-    AIC = -2*ll + 2     *p
-    BIC = -2*ll + log(n)*p
+    AIC = - 2 * ll + 2      * p
+    BIC = - 2 * ll + log(n) * p
 
     return summary_table, ll, AIC, BIC
 end
